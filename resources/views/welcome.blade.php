70727178284f9fe8f043397bf5cc94fb23a7bf35
Magnusser <magnus.roland.raadla@gmail.com>
2024-10-03 11:28:38 +0300

    dawww

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,20 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
+
+  <style>
+    .btn {
+        
+    }
+    .bg-red-600 {
+
+    }
+    .bg-red-600:hover {
+        background-color: yellow;
+    }
+</style>
 </head>
 <body>
-    <button type="button" class="btn btn-blue">Press</button>
-        <style>
-            .btn {
-
-            }
-            .btn-blue {
-
-            }
-            .btn-blue:hover {
-                
-            }
-        </style>
+    <button type="button" class="btn bg-red-600">Press</button>
 </body>
 </html>
