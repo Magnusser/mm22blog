48123a5adc5ebd0e35805bd1b582835b9ccaec23
Magnusser <magnus.roland.raadla@gmail.com>
2025-05-23 16:00:41 +0300

    Removed components

--- resources/views/components/primary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-5 py-2.5 bg-blue-300 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:bg-blue-500 active:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-1 transition ease-in-out duration-200']) }}>
-    {{ $slot }}
-</button>
